f2e652fa28a8dd7a423b5cdc54adbef77233750d
Acetilsalicilic <1Uncanalmas@gmail.com>
2024-05-02 22:25:45 -0700

    Added a file with the links of source images

---
+++ src/assets/imageSource.ts
@@ -0,0 +1,9 @@
+const imageSource = [
+  "https://images.pexels.com/photos/2600964/pexels-photo-2600964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/21945939/pexels-photo-21945939/free-photo-of-mujer-sentado-modelo-maqueta.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/20875624/pexels-photo-20875624/free-photo-of-mar-amanecer-paisaje-puesta-de-sol.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/22812329/pexels-photo-22812329/free-photo-of-ciudad-carretera-trafico-calle.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/22602074/pexels-photo-22602074/free-photo-of-rojo-calle-buzon-urbano.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+
+export default imageSource;
